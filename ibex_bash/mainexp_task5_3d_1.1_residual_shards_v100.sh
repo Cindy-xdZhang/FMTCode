c2081922b09ec9e55f1d2165d820f83183f6efc8
xingdi zhang <xingdi.zhang@kaust.edu.sa>
2026-08-26 16:29:14 +0300

    cluster: use available debug V100 for Task5

--- ibex_bash/mainexp_task5_3d_1.1_residual_shards_v100.sh
+++ ibex_bash/mainexp_task5_3d_1.1_residual_shards_v100.sh
@@ -4,8 +4,8 @@
 #SBATCH -J FMTT5m11res
 #SBATCH -o slurm_logs/%x.%A_%a.out
 #SBATCH -e slurm_logs/%x.%A_%a.err
-#SBATCH --time=4:00:00
-#SBATCH -p gpu4
+#SBATCH --time=2:00:00
+#SBATCH -p debug
 #SBATCH --gpus=1
 #SBATCH --cpus-per-gpu=8
 #SBATCH --constraint=v100
