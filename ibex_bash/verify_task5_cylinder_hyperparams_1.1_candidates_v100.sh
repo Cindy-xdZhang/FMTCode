2f832dca0f72678d802face4f991a3a8e6188e47
xingdi zhang <xingdi.zhang@kaust.edu.sa>
2026-08-26 23:08:40 +0300

    task5: use permitted V100 scheduling

--- ibex_bash/verify_task5_cylinder_hyperparams_1.1_candidates_v100.sh
+++ ibex_bash/verify_task5_cylinder_hyperparams_1.1_candidates_v100.sh
@@ -5,7 +5,6 @@
 #SBATCH -o slurm_logs/%x.%A_%a.out
 #SBATCH -e slurm_logs/%x.%A_%a.err
 #SBATCH --time=4:00:00
-#SBATCH -p gpu4
 #SBATCH --gpus=1
 #SBATCH --cpus-per-gpu=8
 #SBATCH --constraint=v100
