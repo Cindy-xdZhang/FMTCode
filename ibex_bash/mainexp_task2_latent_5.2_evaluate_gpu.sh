df9ff1163fb93fb464afd923993bdc2a3896b790
xingdi zhang <xingdi.zhang@kaust.edu.sa>
2026-08-30 17:08:55 +0300

    experiment: shorten Task2 confirmation walltimes

--- ibex_bash/mainexp_task2_latent_5.2_evaluate_gpu.sh
+++ ibex_bash/mainexp_task2_latent_5.2_evaluate_gpu.sh
@@ -4,7 +4,7 @@
 #SBATCH -J FMTT2l52r
 #SBATCH -o slurm_logs/%x.%A_%a.out
 #SBATCH -e slurm_logs/%x.%A_%a.err
-#SBATCH --time=01:00:00
+#SBATCH --time=00:30:00
 #SBATCH --gpus=1
 #SBATCH --cpus-per-gpu=8
 #SBATCH --mem=48G
